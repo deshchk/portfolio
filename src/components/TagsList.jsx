2ee94d05fa61a586be37651219047575d7cea064
Daniel <110098882+deshchk@users.noreply.github.com>
2025-04-07 13:14:04 +0200

    PostCard is now an astro component + added WIP about page

---
+++ src/components/TagsList.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from "preact/hooks"
+import { off, on } from "@scripts/utils"
+
+export default function TagsList({ tags }) {
+  const tagList = useRef(null)
+  const pos = useRef({
+    top: 0, left: 0, x: 0, y: 0
+  })
+
+  useEffect(() => {
+    function handleMove(e) {
+      const dx = e.clientX - pos.current.x
+      const dy = e.clientY - pos.current.y
+
+      tagList.current.scrollTop = pos.current.top - dy
+      tagList.current.scrollLeft = pos.current.left - dx
+    }
+
+    function handleEnd() {
+      tagList.current.style.cursor = 'grab'
+      tagList.current.style.removeProperty('user-select')
+
+      off(document, 'mousemove', handleMove)
+      off(document, 'mouseup', handleEnd)
+    }
+
+    on(tagList.current, 'mousedown', e => {
+      pos.current = {
+        left: tagList.current.scrollLeft,
+        top: tagList.current.scrollTop,
+        x: e.clientX,
+        y: e.clientY
+      }
+
+      tagList.current.style.cursor = 'grabbing'
+      tagList.current.style.userSelect = 'none'
+
+      on(document, 'mousemove', handleMove)
+      on(document, 'mouseup', handleEnd)
+    })
+  }, [])
+
+  return (
+    <ul
+      ref={tagList}
+      className="[container-type:inline-size] group flex gap-x-2 text-xs overflow-auto invisible-scroll mt-auto pr-12 cursor-grab mask-r-from-black mask-r-from-80% mask-r-to-transparent focus:outline-none"
+    >
+      {tags.map(tag => (
+        <li className="shrink-0 px-2.5 py-0.5 bg-green-light/60 rounded-full group-focus:bg-green-light">{tag}</li>
+      ))}
+    </ul>
+  )
+}
